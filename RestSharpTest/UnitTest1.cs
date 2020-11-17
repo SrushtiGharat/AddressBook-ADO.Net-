986662e432fe42acb4aebf83ac862ccf73b5514c
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-17 12:28:50 +0530

    [Srushti]Add.Multiple contacts to json server

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -3,6 +3,7 @@ using RestSharp;
 using AddressBook_ADO.Net_;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RestSharpTest
 {
@@ -35,6 +36,43 @@ namespace RestSharpTest
             List<Contact> dataResponse = JsonConvert.DeserializeObject<List<Contact>>(response.Content);
 
             Assert.AreEqual(9, dataResponse.Count);
+        }
+
+        /// <summary>
+        /// Add multiple Contacts to json server and check count
+        /// </summary>
+        [TestMethod]
+        public void GivenMultipleContacts_OnPost_ShouldReturn_TotalCount()
+        {
+            List<Contact> contactList = new List<Contact>();
+            contactList.Add(new Contact { FirstName = "Sara", LastName = "Pandit", Address = "21-Jump Street",ZipCode = "300200", City = "Ahmedabad", State = "Gujarat",PhoneNo = "9120384565",Email = "sara@gmail.com",Type = "Friends"});
+            contactList.Add(new Contact { FirstName = "Rohit", LastName = "Sankhe", Address = "27-Dalal Tower", ZipCode = "401501", City = "Mumbai", State = "Maharashtra", PhoneNo = "9999988888", Email = "rohit@gmail.com", Type = "Profession"});
+
+            foreach (Contact contact in contactList)
+            {
+                RestRequest request = new RestRequest("/Contacts", Method.POST);
+                JObject jObjectbody = new JObject();
+                jObjectbody.Add("FirstName", contact.FirstName);
+                jObjectbody.Add("LastName", contact.LastName);
+                jObjectbody.Add("Address", contact.Address);
+                jObjectbody.Add("ZipCode", contact.ZipCode);
+                jObjectbody.Add("City", contact.City);
+                jObjectbody.Add("State", contact.State);
+                jObjectbody.Add("PhoneNo", contact.PhoneNo);
+                jObjectbody.Add("Email", contact.Email);
+                jObjectbody.Add("Type", contact.Type);
+                
+                request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+
+                IRestResponse response = client.Execute(request);
+
+                Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            }
+
+            IRestResponse c = getContactList();
+            List<Contact> dataResponse = JsonConvert.DeserializeObject<List<Contact>>(c.Content);
+
+            Assert.AreEqual(11, dataResponse.Count);
         }
     }
 }
